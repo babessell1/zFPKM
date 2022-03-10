4c92fe80ff3fee6d068abfc94c5c0e3a6eeb83d1
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-03-10 16:09:28 -0500

    Update zfpkm.R

--- R/zfpkm.R
+++ R/zfpkm.R
@@ -174,7 +174,7 @@ zFPKMCalc <- function(fpkm) {
   d <- density(fpkmLog2)
   
   # calculate rolling average
-  perc2 <- as.integer(.02*length(d[["y"]]) + 1
+  perc2 <- as.integer(.02*length(d[["y"]]) + 1)
   f_2perc <- rep(1/perc2, perc2)
   
   d[["roll_y"]] <- filter(as.numeric(d[["y"]]), f_2perc, sides=2)
