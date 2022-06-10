f2182d98ace4a643ee24f5ff66d88b6f117f34c3
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-06-10 14:12:34 -0400

    Update zfpkm.R

--- R/zfpkm.R
+++ R/zfpkm.R
@@ -193,7 +193,7 @@ zFPKMCalc <- function(fpkm, min_thresh) {
   }
 
   local_maxes <- find_maxima(d[["roll_y"]])
-  local_maxes <- find_maxima(d[["roll_y"]][floor(0.01length(d[["roll_y"]])):ceiling(0.9*length(d[["roll_y"]]))])
+  local_maxes <- find_maxima(d[["roll_y"]][floor(0.01*length(d[["roll_y"]])):ceiling(0.9*length(d[["roll_y"]]))])
   print(local_maxes)
   fit_max <- max(local_maxes) + as.integer(perc/2)
 
